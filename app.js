f4994bc702898e032a30b033ee733286326a5549
Rar3Pineapple <108037277+Rar3Pineapple@users.noreply.github.com>
2022-06-27 01:27:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const menu = document.querySelector("#mobilemenu")
 const menuLinks = document.querySelector(".navbar__menu")
-url = "https://api.ipify.org/?format=json";
-function ip() {
-    fetch(url).then(res => results.json()).then(result => {console.log(result)}));
-}
 
 menu.addEventListener("click", function() {
     menu.classList.toggle("is-active");
